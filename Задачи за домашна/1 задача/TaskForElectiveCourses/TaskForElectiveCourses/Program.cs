e328c92a34ff8f71f1f96fc9819bec25888c402f
aleksander0047 <65902735+aleksander0047@users.noreply.github.com>
2020-05-25 15:36:08 +0300

    initial commit

---
+++ Задачи за домашна/1 задача/TaskForElectiveCourses/TaskForElectiveCourses/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskForElectiveCourses
+{
+    class StudentInfo
+    {
+        public string NameOfTheStudent { get; set; }
+        public int FacultyNum { get; set; }
+        public string NameOfTheElectiveCourse { get; set; }
+        public Dictionary<string, double> NameOfTheElectiveCourseAndGrade { get; set; }
+        public double Grades
+        {
+            get
+            {
+                return this.NameOfTheElectiveCourseAndGrade.Max(x => x.Value);
+            }
+        }
+        
+        public StudentInfo()
+        {
+            this.NameOfTheElectiveCourseAndGrade = new Dictionary<string, double>(); 
+        }
+        public override string ToString()
+        {
+            return $"{this.NameOfTheStudent}, {this.NameOfTheElectiveCourse}, {this.Grades}";
+        }
+    }
+   
+    class Program
+    { 
+
+        static void Main(string[] args)
+        {
+       
+            Console.WriteLine("Enter student information:");
+            Console.WriteLine("Name of elective course => Name of the student => faculty number => grade:");
+            List<StudentInfo> studentinfo = new List<StudentInfo>();
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if(input=="end")
+                {
+                    break;
+                }
+                string[] data = input.Split(new string[] { "=>" }, StringSplitOptions.RemoveEmptyEntries).Select(str => str.Trim()).ToArray();
+                string electivecoursename = data[0];
+                string studentname = data[1];
+                int facultynumber = int.Parse(data[2]);
+                double grade = double.Parse(data[3]);
+                if (grade >= 2 && grade <= 6)
+                {
+                    StudentInfo s = studentinfo.Where(stu => stu.NameOfTheStudent.Equals(studentname)).FirstOrDefault();
+                    if (s == null)
+                    {
+                        s = new StudentInfo() { NameOfTheStudent = studentname, FacultyNum = facultynumber, NameOfTheElectiveCourse = electivecoursename};
+                        s.NameOfTheElectiveCourseAndGrade.Add(electivecoursename, grade);
+                        studentinfo.Add(s);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The entered grades are not in the range between 2 and 6!");
+                }
+            }
+
+            Console.WriteLine("Students information:");
+            foreach (StudentInfo studentInfo in studentinfo.OrderByDescending(x => x.Grades))
+            {
+                Console.WriteLine(studentInfo.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Задачи за домашна/2 задача/TaskForACarRace/TaskForACarRace/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskForACarRace
+{
+    class CarRace
+    {
+        public string FirstNameOfTheCompetitor { get; set; }
+        public string LastNameOfTheCompetitor { get; set; }
+        public string CarBrand { get; set; }
+        public double VehicleWeight { get; set; }
+        public int Horsepower { get; set; }
+        public string Boost { get; set; }
+        public Dictionary<string, int> FirstNameOfTheCompetitorAndPoints { get; set; }
+        public int AllPoints
+        {
+            get
+            {
+                return this.FirstNameOfTheCompetitorAndPoints.Sum(x => x.Value);
+            }
+        }
+        public CarRace()
+        {
+            this.FirstNameOfTheCompetitorAndPoints = new Dictionary<string, int>();
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstNameOfTheCompetitor} - {this.AllPoints} points";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter details for the competitor:");
+            Console.WriteLine("First name -> Last name -> Car brand -> Vehicle weight -> Horsepower -> BOOST:");
+            List<CarRace> carracer = new List<CarRace>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                string[] data = input.Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries).Select(str => str.Trim()).ToArray();
+                string firstname = data[0];
+                string lastname = data[1];
+                string carbrand = data[2];
+                double vehicleweight = double.Parse(data[3]);
+                int horsepower = int.Parse(data[4]);
+                string boost = data[5];
+                int points = 0;
+                CarRace r = carracer.Where(rac => rac.FirstNameOfTheCompetitor.Equals(firstname)).FirstOrDefault();
+                if (r == null)
+                {
+                    r = new CarRace() { FirstNameOfTheCompetitor = firstname, LastNameOfTheCompetitor = lastname, CarBrand = carbrand, VehicleWeight = vehicleweight, Horsepower = horsepower, Boost = boost };
+                    r.FirstNameOfTheCompetitorAndPoints.Add(firstname, points);
+                    carracer.Add(r);
+                    if (boost == "do-100")
+                    {
+                        double acceleration = (1 / (horsepower) * 1000 / 2) - (0.30 * 100);
+                    }
+                    else if (boost == "do-200")
+                    {
+                        double acceleration = (1 / (horsepower) * 1000) - (0.20 * 100);
+                    }
+                    else
+                    {
+                        double acceleration = (1 / (horsepower) * 1000);
+                    }
+
+                }
+
+
+                foreach (CarRace carRacer in carracer.OrderByDescending(x => x.AllPoints))
+                {
+                    Console.WriteLine(carRacer.ToString());
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+ 
+
